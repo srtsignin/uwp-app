4bf0999bb9a9a89f3a5b867c18b59f5db095a8ee
Connor Boyle <cjblink1@gmail.com>
2018-10-16 23:17:34 -0400

    UWP app initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+
+import { NavbarModule } from '@srtsignin/navbar';
+import { LoginModule, LoginComponent, LoginService } from '@srtsignin/login';
+import { StudentModule, StudentComponent } from '@srtsignin/student';
+import { RosefireAdapterService } from '@srtsignin/rosefire-adapter';
+import { RolesAdapterService } from '@srtsignin/roles-adapter';
+import { ActiveUsersService } from '@srtsignin/active-users';
+import { CoursesService } from '@srtsignin/courses';
+
+const appRoutes: Routes = [
+  { path: '',   redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component:  LoginComponent},
+  { path: 'student', component:  StudentComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NavbarModule,
+    LoginModule,
+    StudentModule
+  ],
+  providers: [
+    LoginService,
+    RosefireAdapterService,
+    RolesAdapterService,
+    ActiveUsersService,
+    CoursesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
